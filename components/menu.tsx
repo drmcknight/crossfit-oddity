ec845010212a0b554331d3afdfa79518e2fa9200
Daniel McKnight <daniel.r.mcknight@gmail.com>
2022-10-09 14:50:02 -0500

    change icon size

--- components/menu.tsx
+++ components/menu.tsx
@@ -63,7 +63,7 @@ export const Menu = () => {
               <Link href="/crossfit-oddity/schedule">Schedule</Link>
             </MenuLinks>
         </Nav>
-        <FontAwesomeIcon icon={faBars} size="2xs" color='black' style={{display: isMobile ? 'block' : 'none', zIndex: 1, padding: '10px'}} onClick={() => openMenu(!isOpen)} />
+        <FontAwesomeIcon icon={faBars} size="sm" color='black' style={{display: isMobile ? 'block' : 'none', zIndex: 1, padding: '10px'}} onClick={() => openMenu(!isOpen)} />
       </React.Fragment>
     );
 }
